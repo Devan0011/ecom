26836737ea36592f7f1524e51806680c918dadbb
Devan0011 <mahadevan.dvn2009@gmail.com>
2026-04-12 19:41:45 +0530

    Fix production auth CORS and error handling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>,
 )
